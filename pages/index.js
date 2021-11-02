97f81c52e004237247f47dfd048e059bd74d50b9
SamanAtashi <milady00126@gmail.com>
2021-11-02 09:56:56 +0300

    Add components

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
 export default function Home() {
-	return (
-		<div className="bg-Primary w-screen h-screen py-7 relative">
-			
-		</div>
-	);
+	return <div className="bg-Primary w-screen h-screen "></div>;
 }
