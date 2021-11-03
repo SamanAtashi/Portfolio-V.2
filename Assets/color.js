57e0c77daeafa3a0e8a07e3e8fa3660df8002a35
SamanAtashi <milady00126@gmail.com>
2021-11-03 18:47:36 +0300

    Update mobile version of Home page + color.js

---
+++ Assets/color.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const color = (num = 0) => {
+	let arr = ['.', ',', "'"];
+	return <span className="text-Tertiary_Light">{arr[num]}</span>;
+};
+
+export default color;
--- Components/Home.jsx
+++ Components/Home.jsx
@@ -1,28 +1,30 @@
+import color from '../Assets/color';
 const Home = () => {
 	return (
-		<section className="text-Secondary text-[1.2rem] leading-7 font-Montserratss">
-			<article className="">
-				<header className="my-3.5">
-					Hey, I'm{' '}
+		<section className="text-Secondary text-[1.2rem] leading-7 font-Montserratss mt-8">
+			<article>
+				<header className="mb-3.5">
+					Hey{color(1)} I{color(2)}m{' '}
 					<span className="text-Secondary_Light font-semibold">
 						Saman
 					</span>
-					.
+					{color()}
 				</header>
 				<p>
-					I'm a{' '}
+					I{color(2)}m a{' '}
 					<span className="text-Secondary_Light font-semibold">
 						FrontEnd
 					</span>{' '}
-					developer who loves solving everyday life's
-					problems with code.
+					developer who loves solving everyday life
+					{color(2)}s problems with code
+					{color()}
 				</p>
 				<p className="text-sm my-3.5">
 					Currently a student at{' '}
 					<span className="text-Secondary_Light font-semibold">
 						Microverse
 					</span>
-					.
+					{color()}
 				</p>
 			</article>
 		</section>
