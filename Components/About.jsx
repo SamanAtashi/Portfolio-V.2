53febd4f356d853c319df0611173b3929d20d021
SamanAtashi <milady00126@gmail.com>
2021-11-04 11:54:21 +0300

    Add react icons

--- Components/About.jsx
+++ Components/About.jsx
@@ -1,4 +1,5 @@
 import color from '../Assets/color';
+import { ImQuotesLeft, ImQuotesRight } from 'react-icons/im/';
 
 const About = () => {
 	return (
@@ -7,30 +8,43 @@ const About = () => {
 				<span>About</span>
 			</header>
 			<article className="my-8 font-Montserratss">
-				<span className="text-Tertiary_Light font-Bodoni text-3xl">
-					“
-				</span>
 				<p>
+					<div className="inline-block">
+						<ImQuotesLeft className="text-Tertiary text-xl" />
+					</div>{' '}
 					I{color(2)}m a{' '}
 					<span className="font-bold text-Secondary_Light my_underline">
 						Software Developer
 					</span>{' '}
 					based in Russia
 					{color()} I started my coding journey almost{' '}
-					<span className="font-bold text-Secondary_Light my_underline">1{color(1)}5</span>{' '}
+					<span className="font-bold text-Secondary_Light my_underline">
+						1{color(1)}5
+					</span>{' '}
 					years ago{color()}
 				</p>
 				<p className="my-3.5">
 					I love both the{' '}
-					<span className="font-bold text-Secondary_Light my_underline">creative</span> and{' '}
-					<span className="font-bold text-Secondary_Light my_underline">analytical</span>{' '}
+					<span className="font-bold text-Secondary_Light my_underline">
+						creative
+					</span>{' '}
+					and{' '}
+					<span className="font-bold text-Secondary_Light my_underline">
+						analytical
+					</span>{' '}
 					sides of development{color()} You may have noticed
 					from my portfolio that I{color(2)}m obsessed with{' '}
-					<span className="font-bold text-Secondary_Light my_underline">unique</span>
+					<span className="font-bold text-Secondary_Light my_underline">
+						unique
+					</span>
 					{color(1)}{' '}
-					<span className="font-bold text-Secondary_Light my_underline">simple</span>
+					<span className="font-bold text-Secondary_Light my_underline">
+						simple
+					</span>
 					{color(1)} and{' '}
-					<span className="font-bold text-Secondary_Light my_underline">user-friendly</span>{' '}
+					<span className="font-bold text-Secondary_Light my_underline">
+						user-friendly
+					</span>{' '}
 					functionality{color()}
 				</p>
 				<p>
@@ -41,22 +55,30 @@ const About = () => {
 					{color(1)} I specialize in a multi-disciplined
 					approach to my work{color(1)} incorporating
 					{color(7)}{' '}
-					<span className="font-bold text-Secondary_Light my_underline">Troubleshooting</span>
+					<span className="font-bold text-Secondary_Light my_underline">
+						Troubleshooting
+					</span>
 					{color(1)}{' '}
 					<span className="font-bold text-Secondary_Light my_underline">
 						Relentless Debugging
 					</span>
 					{color(1)}{' '}
-					<span className="font-bold text-Secondary_Light my_underline">Adaptability</span>
+					<span className="font-bold text-Secondary_Light my_underline">
+						Adaptability
+					</span>
 					{color(1)}{' '}
-					<span className="font-bold text-Secondary_Light my_underline">Analytic</span>
+					<span className="font-bold text-Secondary_Light my_underline">
+						Analytic
+					</span>
 					{color(1)} and{' '}
-					<span className="font-bold text-Secondary_Light my_underline">Proactivity</span>
-					{color()}
+					<span className="font-bold text-Secondary_Light my_underline">
+						Proactivity
+					</span>
+					{color()}{' '}
+					<div className="inline-block">
+						<ImQuotesRight className="text-Tertiary text-xl" />
+					</div>
 				</p>
-				<span className="text-Tertiary_Light font-Bodoni text-3xl ">
-					”
-				</span>
 			</article>
 			<div className="sm:flex sm:justify-between">
 				<article className="my-4 ">
