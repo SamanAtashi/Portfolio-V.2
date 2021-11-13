20c9fd6fb0d84fd884fe7805628ef3ee04a73c9d
SamanAtashi <milady00126@gmail.com>
2021-11-13 09:18:05 +0300

    Update meta

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import Head from 'next/head';
 import About from '../Components/About/About';
 import Contact from '../Components/Contacts';
 import Home from '../Components/Home';
@@ -6,6 +7,7 @@ import Home from '../Components/Home';
 import magnetCurs from '../Components/Mouse/magnetCurs';
 import Navbar from '../Components/Navbar';
 import Projects from '../Components/Projects/Projects';
+import sc from '../public/sc.png';
 
 export default function Main() {
 	useEffect(() => {
@@ -17,13 +19,52 @@ export default function Main() {
 	}, []);
 
 	return (
-		<div className="bg-Primary w-full h-full text-Secondary px-10vw pt-6  pb-10 relative md:px-28 md:py-12 lg:pt-0 lg:px-12vw lg:pb-28 xl:px-16vw">
-			{/* <DotRing /> */}
-			<Navbar />
-			<Home />
-			<Projects />
-			<About />
-			<Contact />
-		</div>
+		<>
+			<Head>
+				<title>Saman Atashi</title>
+				<meta
+					name="description"
+					content="Saman Atashi's Portfolio"
+				/>
+
+				<meta itemProp="name" content="Saman Atashi" />
+				<meta
+					itemProp="description"
+					content="Saman Atashi's Portfolio"
+				/>
+				<meta itemProp="image" content={sc} />
+
+				<meta
+					property="og:url"
+					content="https://samanatashi.netlify.app/"
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="Saman Atashi" />
+				<meta
+					property="og:description"
+					content="Saman Atashi's Portfolio"
+				/>
+				<meta property="og:image" content={sc} />
+
+				<meta
+					name="twitter:card"
+					content="summary_large_image"
+				/>
+				<meta name="twitter:title" content="Saman Atashi" />
+				<meta
+					name="twitter:description"
+					content="Saman Atashi's Portfolio"
+				/>
+				<meta name="twitter:image" content={sc} />
+			</Head>
+			<div className="bg-Primary w-full h-full text-Secondary px-10vw pt-6  pb-10 relative md:px-28 md:py-12 lg:pt-0 lg:px-12vw lg:pb-28 xl:px-16vw">
+				{/* <DotRing /> */}
+				<Navbar />
+				<Home />
+				<Projects />
+				<About />
+				<Contact />
+			</div>
+		</>
 	);
 }
